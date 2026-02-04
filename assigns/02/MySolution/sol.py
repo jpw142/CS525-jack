ef682495660cddbba20ed36f51035f76fa2c43f2
Jack Weber <jrw142@bu.edu>
2026-02-03 20:57:34 -0500

    done

---
+++ assigns/02/MySolution/sol.py
@@ -0,0 +1,160 @@
+############################################################
+#
+# Assign01 for CS525, Spring, 2026
+# It is due the 27th of January, 2026
+# Note that the due time is always 11:59pm of
+# the due date unless is specified otherwise.
+#
+############################################################
+# datatype term =
+# | TMvar of strn
+# | TMlam of (strn, term)
+# | TMapp of (term, term)
+############################################################
+#
+class term:
+    ctag = ""
+# end-of-class(term)
+#
+class term_var(term):
+    def __init__(self, arg1):
+        self.arg1 = arg1
+        self.ctag = "TMvar"
+    def __str__(self):
+        return ("TMvar(" + self.arg1 + ")")
+# end-of-class(term_var(term))
+#
+class term_lam(term):
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.ctag = "TMlam"
+    def __str__(self):
+        return ("TMlam(" + self.arg1 + "," + str(self.arg2) + ")")
+# end-of-class(term_lam(term))
+#
+class term_app(term):
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.ctag = "TMapp"
+    def __str__(self):
+        return ("TMapp(" + str(self.arg1) + "," + str(self.arg2) + ")")
+# end-of-class(term_app(term))
+#
+############################################################
+#
+def TMvar(x00):
+    return term_var(x00)
+def TMlam(x00, tm1):
+    return term_lam(x00, tm1)
+def TMapp(tm1, tm2):
+    return term_app(tm1, tm2)
+#
+############################################################
+#
+def I_():
+    x = TMvar("x")
+    return TMlam("x", x)
+def K_():
+    x = TMvar("x")
+    return TMlam("x", TMlam("y", x))
+def S_():
+    x = TMvar("x")
+    y = TMvar("y")
+    z = TMvar("z")
+    return TMlam("x", TMlam("y", TMlam("z", TMapp(TMapp(x, z), TMapp(y, z)))))
+#
+_ = print("I =", I_())
+_ = print("K =", K_())
+_ = print("S =", S_())
+#
+############################################################
+#
+# HX: [sub] should be closed!
+#
+def term_subst0(tm0, x00, sub):
+    def subst0(tm0):
+        if (tm0.ctag == "TMvar"):
+            x01 = tm0.arg1
+            return sub if (x00 == x01) else tm0
+        if (tm0.ctag == "TMlam"):
+            x01 = tm0.arg1
+            if (x00 == x01):
+                return tm0
+            else:
+                return TMlam(x01, subst(tm0.arg2))
+        if (tm0.ctag == "TMapp"):
+            return TMapp(subst(tm0.arg1), subst(tm0.arg2))
+        raise TypeError(tm0) # HX: should be deadcode!
+    return subst0(tm0)
+#
+############################################################
+
+def term_freevars(tm0):
+    if tm0.ctag == "TMvar":
+        return {tm0.arg1}
+    if tm0.ctag == "TMlam":
+        x01 = tm0.arg1
+        body_fvs = term_freevars(tm0.arg2)
+        return body_fvs - {x01}
+    if tm0.ctag == "TMapp":
+        fv1 = term_freevars(tm0.arg1)
+        fv2 = term_freevars(tm0.arg2)
+        return fv1 | fv2
+    raise TypeError(tm0)
+#
+############################################################
+#
+def term_freshvar(avoid_set):
+    i = 0
+    while True:
+        candidate = f"x{i}"
+        if candidate not in avoid_set:
+            return candidate
+        i += 1
+
+############################################################
+def term_gsubst(tm0, x00, sub):
+    """
+    Points: 20
+    This function implements the (general) substitution function on terms
+    that should correctly handle an open [sub] (that is, [sub] containing 
+    free variables)
+    You can use the function [term_freevars] implemented in Assign01.
+    """
+    if tm0.ctag == "TMvar":
+        x01 = tm0.arg1
+        return sub if (x00 == x01) else tm0
+    
+    if tm0.ctag == "TMapp":
+        tm1_new = term_gsubst(tm0.arg1, x00, sub)
+        tm2_new = term_gsubst(tm0.arg2, x00, sub)
+        return TMapp(tm1_new, tm2_new)
+    
+    if tm0.ctag == "TMlam":
+        x01 = tm0.arg1
+        body = tm0.arg2
+        
+        if x00 == x01:
+            return tm0
+        
+        sub_fvs = term_freevars(sub)
+        
+        if x01 in sub_fvs:
+            body_fvs = term_freevars(body)
+            avoid = sub_fvs | body_fvs | {x00, x01}
+            fresh = term_freshvar(avoid)
+            renamed_body = term_subst0(body, x01, TMvar(fresh))
+            new_body = term_gsubst(renamed_body, x00, sub)
+            return TMlam(fresh, new_body)
+        else:
+            new_body = term_gsubst(body, x00, sub)
+            return TMlam(x01, new_body)
+    
+    raise TypeError(tm0)
+
+############################################################
+# end of [HWXI/CS525-2026-Fall/assigns/02/lambda0.py]
+############################################################
+############################################################
